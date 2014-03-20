96d6d9e5b83924936436774f148fa0028ebae431
transportwaker <transportwaker@gmail.com>
2014-03-19 22:23:08 -0400

    homepage

--- TransportWaker/MainPage.xaml.cs
+++ TransportWaker/MainPage.xaml.cs
@@ -22,8 +22,7 @@ namespace TransportWaker
             // Set the data context of the LongListSelector control to the sample data
             DataContext = App.ViewModel;
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
+            BuildLocalizedApplicationBar();
         }
 
         // Load data for the ViewModel Items
@@ -36,33 +35,35 @@ namespace TransportWaker
         }
 
         // Handle selection changed on LongListSelector
-        private void MainLongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        //private void MainLongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+       // {
             // If selected item is null (no selection) do nothing
-            if (MainLongListSelector.SelectedItem == null)
-                return;
-
+          //  if (MainLongListSelector.SelectedItem == null)
+         //       return;
+        //
             // Navigate to the new page
-            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + (MainLongListSelector.SelectedItem as ItemViewModel).ID, UriKind.Relative));
+        //    NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + (MainLongListSelector.SelectedItem as ItemViewModel).ID, UriKind.Relative));
 
             // Reset selected item to null (no selection)
-            MainLongListSelector.SelectedItem = null;
+         //   MainLongListSelector.SelectedItem = null;
+       // }
+
+
+        private void BuildLocalizedApplicationBar()
+        {
+            ApplicationBar = new ApplicationBar();
+
+            ApplicationBarMenuItem aboutAppBar =
+                new ApplicationBarMenuItem();
+            aboutAppBar.Text = AppResources.AppBarAbout;
+
+            aboutAppBar.Click += aboutAppBar_Click;
+
+            ApplicationBar.MenuItems.Add(aboutAppBar);
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void aboutAppBar_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
