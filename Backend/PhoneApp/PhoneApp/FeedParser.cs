cbee3278dbd0d63e050eba44756428194301d456
Ben Hudson <ben@hudsonsathome.com>
2014-03-27 09:11:36 -0700

    set up and added async methods

---
+++ Backend/PhoneApp/PhoneApp/FeedParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace PhoneApp
+{
+    class FeedParser
+    {
+        private string agency;
+
+        public FeedParser(string agency)
+        {
+            this.agency = agency;
+        }
+
+        private static async Task<string> DownloadString(string url)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(new Uri("http://www.microsoft.com"));
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetRouteList()
+        {
+            string data = await DownloadString("http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=" + agency);
+            XDocument root = XDocument.Parse(data);
+            List<XElement> routes = new List<XElement>();
+            routes.AddRange(root.Descendants("route"));
+
+            if (routes.Count == 0)
+            {
+                return "[{\"error\": \"No routes found\"}]";
+            }
+
+            string json = "[ {\"tag\": \"" + routes[0].Attribute("tag").Value + "\", \"name\": \"" + routes[0].Attribute("title").Value + "\"}";
+            for (int i = 1; i < routes.Count; i++)
+            {
+                json += ", {\"tag\": \"" + routes[i].Attribute("tag").Value + "\", \"name\": \"" + routes[i].Attribute("title").Value + "\"}";
+            }
+            json += "]";
+
+            Debug.WriteLine(json);
+            return json;
+        }
+    }
+}
---
+++ Backend/PhoneApp/PhoneApp/Location.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneApp
+{
+    class Location
+    {
+        public double Latitude;
+        public double Longitude;
+
+        public Location(double Latitude, double Longitude)
+        {
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+        }
+
+        public double getDistance(Location to)
+        {
+            double dLat = (this.Latitude - to.Latitude) * Math.PI / 180;
+            double dLon = (this.Longitude - to.Longitude) * Math.PI / 180;
+            double lat1 = this.Latitude * Math.PI / 180;
+            double lat2 = to.Latitude * Math.PI / 180;
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Sin(dLon / 2)
+            * Math.Sin(dLon / 2) * Math.Cos(lat1) * Math.Cos(lat2);
+            return 6371 * 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        }
+    }
+}
